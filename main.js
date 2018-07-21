494d550388860c70352f5f2071da950a2fd15971
Sakshi <sakshisrivastava413@gmail.com>
2018-07-21 13:13:18 +0530

    added keyboard controls

--- main.js
+++ main.js
@@ -24,18 +24,37 @@ var gameState = {
         ledge.body.immovable = true;
         ledge = platforms.create(-150, 250, 'ground');
         ledge.body.immovable = true;
-        
+
 
         player = game.add.sprite(32, game.world.height - 150, 'dude');
         game.physics.arcade.enable(player);
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
-        player.animations.add('right', [5, 6, 7, 8], 10, true);    
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
         player.animations.add('left', [0, 1, 2, 3], 10, true);
     },
     update: function () {
         var hitPlatform = game.physics.arcade.collide(player, platforms);
+        var cursors = game.input.keyboard.createCursorKeys();
+
+
+        player.body.velocity.x = 0;
+        if (cursors.left.isDown) {
+            player.body.velocity.x = -150;
+            player.animations.play('left');
+        }
+        else if (cursors.right.isDown) {
+            player.body.velocity.x = 150;
+            player.animations.play('right');
+        }
+        else {
+            player.animations.stop();
+            player.frame = 4;
+        }
+        if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
+            player.body.velocity.y = -350;
+        }
 
 
     }
